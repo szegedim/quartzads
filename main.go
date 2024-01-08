8b88323751a2695601b356099157df9853418c18
Miklos Szegedi <hq@opensource.eper.io>
2024-01-07 16:29:26 -0800

    work

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ import (
 //You should have received a copy of the CC0 Public Domain Dedication along with this software.
 //If not, see <https:#creativecommons.org/publicdomain/zero/1.0/legalcode>.
 
+// Test:
+// IMPLEMENTATION=https://gitlab.com/-/snippets/3634913/raw/main/Function go run main.go
 func main() {
 	cards.Setup()
 
